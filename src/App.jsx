b2ec68f041b0c6b75d9611d336fb315450320ce6
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-08-28 18:12:52 +0530

    tailwind setup created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-
 function App() {
-
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 className="text-3xl font-bold underline text-red-600 ">
+        Hello world!
+      </h1>
     </>
   );
 }
