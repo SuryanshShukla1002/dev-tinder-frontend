4cc9d03c5dec148047e1bb6f0831b7c9a4a426a4
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-08-29 23:59:32 +0530

    components created for Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./NavBar";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-600 ">
-        Hello world!
-      </h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
