941edc20a8163e80d7a2e72ca661c8a38af261d1
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-08-31 20:29:35 +0530

    created login and added redux to store data

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,67 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "./utils/userSlice";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("suryansh847@gmail.com");
+  const [password, setPassword] = useState("Surya123@");
+  const dispatch = useDispatch();
+
+  const handleLoginClick = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:7777/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true } // it will not send the token passed if not passed
+      );
+      // console.log(res);
+      dispatch(addUser(res.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title  justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Email ID</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary" onClick={handleLoginClick}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Login;
