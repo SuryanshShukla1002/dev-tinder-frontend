d3dcc1a8724a8a6ff50bfd3b1ba2c33224b7c66a
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-09-08 23:08:20 +0530

    handling error case

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addConnections } from "../utils/connectionSlice";
 
 const Connections = () => {
   const connections = useSelector((store) => store.connections);
   const dispatch = useDispatch();
+  const [error, setError] = useState("");
   const fetchConnections = async () => {
     try {
       const res = await axios.get(BASE_URL + "/user/connections", {
@@ -14,8 +15,10 @@ const Connections = () => {
       });
       //   console.log(res.data.data);
       dispatch(addConnections(res?.data?.data));
-    } catch (err) {
-      // HANDLE LATER
+      setError("");
+    } catch (error) {
+      console.log("Something went wrong", error);
+      setError(error?.response?.data?.message || "Something went wrong");
     }
   };
 
@@ -23,18 +26,28 @@ const Connections = () => {
     fetchConnections();
   }, []);
 
-  if (!connections) return;
+  if (!connections) {
+    if (error) {
+      return (
+        <h1 className="text-center text-2xl text-red-500 my-10">{error}</h1>
+      );
+    }
+    return null;
+  }
   if (connections.length === 0) return <h1>No Connections found</h1>;
 
   return (
     <div className=" text-center my-10">
       <h1 className="text-3xl text-bold text-white">Connections</h1>
       {connections.map((connection) => {
-        const { firstName, lastName, age, gender, photoUrl, about } =
+        const { _id, firstName, lastName, age, gender, photoUrl, about } =
           connection;
 
         return (
-          <div className="flex items-center gap-4 m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
+          <div
+            key={_id}
+            className="flex items-center gap-4 m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto"
+          >
             <div className="flex-shrink-0">
               <img
                 src={photoUrl}
