b8960c0daeb1ed7f3e682933a0340f85e132ed45
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-09-09 13:28:15 +0530

    fixed error handle and completed profile page

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -8,9 +8,9 @@ import { addUser } from "../utils/userSlice";
 const EditProfile = ({ user }) => {
   const [firstName, setFirstName] = useState(user.firstName);
   const [lastName, setLastName] = useState(user.lastName);
-  const [age, setAge] = useState(user.age);
+  const [age, setAge] = useState(user.age || "");
   const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
-  const [gender, setGender] = useState(user.gender);
+  const [gender, setGender] = useState(user.gender || "");
   const [about, setAbout] = useState(user.about);
   const [error, setError] = useState("");
   const [showToast, setShowToast] = useState(false);
@@ -37,12 +37,13 @@ const EditProfile = ({ user }) => {
         setShowToast(false);
       }, 3000);
     } catch (err) {
-      setError(err.response.data);
+      setError(err.response?.data || "Something went wrong");
     }
   };
 
   return (
     <>
+      <h2 className="text-center text-2xl mt-3">Complete Your Profile!</h2>
       <div className="flex justify-center my-10">
         <div className="flex justify-center mx-10">
           <div className="card bg-base-300 w-96 shadow-sm">
@@ -60,6 +61,7 @@ const EditProfile = ({ user }) => {
                     onChange={(e) => setFirstName(e.target.value)}
                   />
                 </label>
+
                 <label className="form-control w-full max-w-xs my-2">
                   <div className="label">
                     <span className="label-text">Last Name:</span>
@@ -71,6 +73,7 @@ const EditProfile = ({ user }) => {
                     onChange={(e) => setLastName(e.target.value)}
                   />
                 </label>
+
                 <label className="form-control w-full max-w-xs my-2">
                   <div className="label">
                     <span className="label-text">Photo URL:</span>
@@ -82,9 +85,10 @@ const EditProfile = ({ user }) => {
                     onChange={(e) => setPhotoUrl(e.target.value)}
                   />
                 </label>
+
                 <label className="form-control w-full max-w-xs my-2">
                   <div className="label">
-                    <span className="label-text"> Age:</span>
+                    <span className="label-text">Age:</span>
                   </div>
                   <input
                     type="text"
@@ -93,20 +97,26 @@ const EditProfile = ({ user }) => {
                     onChange={(e) => setAge(e.target.value)}
                   />
                 </label>
+
                 <label className="form-control w-full max-w-xs my-2">
                   <div className="label">
                     <span className="label-text">Gender:</span>
                   </div>
-                  <input
-                    type="text"
+                  <select
                     value={gender}
-                    className="input input-bordered w-full max-w-xs mb-2"
                     onChange={(e) => setGender(e.target.value)}
-                  />
+                    className="select select-bordered w-full max-w-xs mb-2"
+                  >
+                    <option value="">Select gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="others">Others</option>
+                  </select>
                 </label>
+
                 <label className="form-control w-full max-w-xs my-2">
                   <div className="label">
-                    <span className="label-text">Abour:</span>
+                    <span className="label-text">About:</span>
                   </div>
                   <input
                     type="text"
@@ -116,7 +126,9 @@ const EditProfile = ({ user }) => {
                   />
                 </label>
               </div>
+
               <p className="text-red-500">{error}</p>
+
               <div className="card-actions justify-center m-2">
                 <button className="btn btn-primary" onClick={saveProfile}>
                   Save Profile
@@ -132,7 +144,7 @@ const EditProfile = ({ user }) => {
       {showToast && (
         <div className="toast toast-top toast-center">
           <div className="alert alert-success">
-            <span>Profile Saved successfully.</span>
+            <span>Profile Saved successfully</span>
           </div>
         </div>
       )}
