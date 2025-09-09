155642bfc696ec3eebb3f4acbe031433bd54fdfb
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-09-09 13:26:33 +0530

    error handled done

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -34,7 +34,12 @@ const Connections = () => {
     }
     return null;
   }
-  if (connections.length === 0) return <h1>No Connections found</h1>;
+  if (connections.length === 0)
+    return (
+      <h1 className="flex justify-center my-10 text-2xl">
+        No Connections found
+      </h1>
+    );
 
   return (
     <div className=" text-center my-10">
