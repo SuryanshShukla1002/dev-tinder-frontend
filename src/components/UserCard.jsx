56b99ea2fc67b2a17db2cb7ec959010f8e3ce286
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-09-15 11:10:00 +0530

    completed User Card showing details done

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,24 @@
-const UserCard = ({ user }) => {
-  const { firstName, lastName, age, gender, photoUrl, about } = user;
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
 
-  
+const UserCard = ({ user }) => {
+  const { _id, firstName, lastName, age, gender, photoUrl, about } = user;
+  const dispatch = useDispatch();
+
+  const handleSendRequest = async (status, userId) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/send/" + status + "/" + userId,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeUserFromFeed(userId));
+    } catch (error) {
+      // Handle later
+    }
+  };
 
   return (
     <div className="card card-compact bg-base-300 w-96  shadow-xl">
@@ -17,8 +34,18 @@ const UserCard = ({ user }) => {
         {age && gender && <p>{age + ", " + gender}</p>}
         <p>{about}</p>
         <div className="card-actions justify-center my-4">
-          <button className="btn btn-primary">Ignore</button>
-          <button className="btn btn-secondary">Interested</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => handleSendRequest("ignored", _id)}
+          >
+            Ignore
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleSendRequest("interested", _id)}
+          >
+            Interested
+          </button>
         </div>
       </div>
     </div>
